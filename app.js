a7111e2cfe62160bc6222e2ea0851c823b111bec
jhars <jonharlan.io@gmail.com>
2018-12-31 18:08:45 -0500

    MVC - user: first_name => henry (user table, psql-DB)

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const app = express(); // Set up the express app
 
-// Set up the express app
-const app = express();
-
-// Log requests to the console.
-app.use(logger('dev'));
-
-// Parse incoming requests data (https://github.com/expressjs/body-parser)
-app.use(bodyParser.json());
+app.use(logger('dev')); // Log requests to the console.
+app.use(bodyParser.json()); // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.urlencoded({ extended: false }));
+require('./server/routes')(app); //Require our routes into the application
 
-// Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
+app.get('*', (req, res) => res.status(200).send({ // Setup a default catch-all route that sends back a welcome message in JSON format.
   message: 'Welcome to the beginning of nothingness.',
 }));
 
