43678eae8284d14b9df60ef4e1d1b09ecfeb114f
jhars <jonharlan.io@gmail.com>
2019-01-21 20:29:44 -0500

    hash + salt password

--- README.md
+++ README.md
@@ -5,8 +5,24 @@
 3) http://localhost:8000/
 
 
+4) $ node server/services/auth_crypt.js
+
+  -> creates hash+salt password :)
+
 
 ## NOTES:
 
 ADD COLUMN TO TABLE:
 sequelize migration:create --name Users
+
+
+
+Dev Strategy:
+
+[1] 2-Factor SMS Authentication (Twillio)
+[2] Create "user_model": {phone #}
+  -> Confirm User via SMS
+[3] Use 'bcrypt' npm pkg to {'hash' + 'salt'} pw
+[4] Facebook Auth
+[5] Reset Password Route
+[6] Linkedin Auth + Google Auth
--- server/services/auth_crypt.js
+++ server/services/auth_crypt.js
@@ -1,14 +1,18 @@
-const bcrypt = requre('bcrypt');
+const bcrypt = require('bcrypt');
 
 bcrypt.hash('myPassword', 10, function(err, hash) {
   //Store hash in database
+  console.log(hash);
+  console.log(err);
+
+
 });
 
 
-bcrypt.compare('somePassword', hash, function(err, res) {
-  if (res) {
-    //passwords match
-  } else {
-    //passwords don't match
-  }
-});
+// bcrypt.compare('somePassword', hash, function(err, res) {
+//   if (res) {
+//     //passwords match
+//   } else {
+//     //passwords don't match
+//   }
+// });
