4039e946c8cb4d2f96258ea79fac468676c83b1b
jhars <jonharlan.io@gmail.com>
2019-01-21 20:10:29 -0500

    Clean VC history

--- server/services/send_sms.js
+++
@@ -1,7 +0,0 @@
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Your Account Sid and Auth Token from twilio.com/console
-const accountSid = 'AC95a7fd667614b732f4323bc082022243';
-const authToken = '2c5fed5d13dbf8195bc8a47913c3a234';
-
-exports.sid = accountSid;
-exports.token = authToken;
